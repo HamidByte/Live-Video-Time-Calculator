379233da4063d26aefae243d70cdea77e8db86e6
hamidbyte <muhammad.hamid44@gmail.com>
2025-02-09 10:39:25 +0100

    fix: unexpected value change for w2 and h2; new ratio preset added and reset button

--- src/components/ui/NumberInput.vue
+++ src/components/ui/NumberInput.vue
@@ -6,25 +6,30 @@ const props = defineProps({
   label: String,
   step: {
     type: Number,
-    default: 1
-  }
-});
+    default: 1,
+  },
+})
 
-const emit = defineEmits(['update:modelValue']);
+const emit = defineEmits(['update:modelValue'])
 
 const decrement = () => {
-  emit('update:modelValue', props.modelValue - props.step);
-};
+  emit('update:modelValue', props.modelValue - props.step)
+}
 
 const increment = () => {
-  emit('update:modelValue', props.modelValue + props.step);
-};
+  emit('update:modelValue', props.modelValue + props.step)
+}
 </script>
 
 <template>
   <div class="input-group w-full" data-input-number>
     <span class="input-group-text border-base-content/25 border-e ps-0">
-      <button type="button" class="flex size-12 items-center justify-center" aria-label="Decrement button" @click="decrement">
+      <button
+        type="button"
+        class="flex size-12 items-center justify-center"
+        aria-label="Decrement button"
+        @click="decrement"
+      >
         <span class="icon-[tabler--minus] size-3.5 flex-shrink-0"></span>
       </button>
     </span>
@@ -35,12 +40,19 @@ const increment = () => {
       type="text"
       aria-label="seat counter"
     />
-    <div class="absolute bottom-1 start-1/2 flex -translate-x-1/2 items-center rtl:translate-x-1/2">
+    <div
+      class="absolute bottom-1 start-1/2 flex -translate-x-1/2 items-center rtl:translate-x-1/2"
+    >
       <!-- <span class="icon-[tabler--ticket] text-base-content/80 me-2"></span> -->
       <span class="text-xs text-base-content/80 text-nowrap">{{ label }}</span>
     </div>
     <span class="input-group-text border-base-content/25 border-s pe-0">
-      <button type="button" class="flex size-12 items-center justify-center" aria-label="Increment button" @click="increment">
+      <button
+        type="button"
+        class="flex size-12 items-center justify-center"
+        aria-label="Increment button"
+        @click="increment"
+      >
         <span class="icon-[tabler--plus] size-3.5 flex-shrink-0"></span>
       </button>
     </span>
